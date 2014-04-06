f5f2aff2001401996a55eb4526aba4e7910ba212
succi0303 <succi0303@gmail.com>
2014-04-06 16:45:22 +0900

    update view static_pages about & contact

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -40,12 +40,40 @@ feature 'index page' do
     expect(page).to have_content('Record Your Pomodori')
   end
 
+  scenario "has the content 'ユーザー登録'" do
+    expect(page).to have_content('ユーザー登録')
+  end
+
 end
 
 feature 'about page' do
 
+  before :each do
+    visit about_path
+  end
+
+  scenario "has the title 'Record Your Pomodori | About'" do
+    expect(page).to have_title('Record Your Pomodori | About')
+  end
+
+  scenario "has the content 'このサイトについて'" do
+    expect(page).to have_content('このサイトについて')
+  end
+
 end
 
 feature 'contact page' do
+
+  before :each do
+    visit contact_path
+  end
+
+  scenario "has the title 'Record Your Pomodori | Contact'" do
+    expect(page).to have_title('Record Your Pomodori | Contact')
+  end
+
+  scenario "has the content 'お問い合わせ'" do
+    expect(page).to have_content('お問い合わせ')
+  end
 
 end
