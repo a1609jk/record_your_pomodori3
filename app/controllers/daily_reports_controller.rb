224491b13138f0c6ad73e82217331f465a389bef
succi0303 <succi0303@gmail.com>
2014-04-09 11:37:21 +0900

    update controller daily_reports, daily_reports create

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -6,4 +6,22 @@ class DailyReportsController < ApplicationController
     @daily_report = @monthly_report.daily_reports.new
     render :new
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @monthly_report = @user.monthly_reports.find(params[:monthly_report_id])
+    @daily_report = @monthly_report.daily_reports.new(daily_report_params)
+
+    if @daily_report.save
+      redirect_to user_monthly_report_path(@user, @monthly_report)
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+    def daily_report_params
+      params.require(:daily_report).permit(:day, :pomodori, :task, :memo)
+    end
 end
--- spec/controllers/daily_reports_controller_spec.rb
+++ spec/controllers/daily_reports_controller_spec.rb
@@ -18,4 +18,48 @@ describe DailyReportsController do
     end
   end
 
+  describe 'POST #create' do
+
+    before :each do
+      @daily_report = build(:daily_report)
+    end
+    
+    context "with valid attributes" do
+      it "saves the new daily report in the database" do
+        expect{
+          post :create, daily_report: attributes_for(:daily_report),
+            monthly_report_id: @daily_report.monthly_report.id,
+            user_id: @daily_report.monthly_report.user.id
+        }.to change(DailyReport, :count).by(1)
+      end
+
+      it "redirects to monthly_reports#show" do
+        post :create, daily_report: attributes_for(:daily_report),
+          monthly_report_id: @daily_report.monthly_report.id,
+          user_id: @daily_report.monthly_report.user.id
+        expect(response).to redirect_to user_monthly_report_path(
+          @daily_report.monthly_report.user,
+          @daily_report.monthly_report
+        )
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the daily report in the database" do
+        expect{
+          post :create, daily_report: attributes_for(:daily_report, day: nil),
+            monthly_report_id: @daily_report.monthly_report.id,
+            user_id: @daily_report.monthly_report.user.id
+        }.to_not change(DailyReport, :count)
+      end
+
+      it "re-renders the :new template" do
+        post :create, daily_report: attributes_for(:daily_report, day: nil),
+          monthly_report_id: @daily_report.monthly_report.id,
+          user_id: @daily_report.monthly_report.user.id
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
 end
