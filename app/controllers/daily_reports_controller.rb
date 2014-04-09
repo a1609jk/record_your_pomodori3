1f63e2a09d32814cbf0bd963086a9670e5b49fca
succi0303 <succi0303@gmail.com>
2014-04-09 15:25:11 +0900

    update controller daily_reports, edit/update & destory

--- app/controllers/daily_reports_controller.rb
+++ app/controllers/daily_reports_controller.rb
@@ -19,6 +19,33 @@ class DailyReportsController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:user_id])
+    @monthly_report = @user.monthly_reports.find(params[:monthly_report_id])
+    @daily_report = @monthly_report.daily_reports.find(params[:id])
+    render action: 'edit'
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @monthly_report = @user.monthly_reports.find(params[:monthly_report_id])
+    @daily_report = @monthly_report.daily_reports.find(params[:id])
+
+    if @daily_report.update(daily_report_params)
+      redirect_to user_monthly_report_path(@user, @monthly_report)
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @monthly_report = @user.monthly_reports.find(params[:monthly_report_id])
+    @daily_report = @monthly_report.daily_reports.find(params[:id])
+    @daily_report.destroy
+    redirect_to user_monthly_report_path(@user, @monthly_report)
+  end
+
   private
 
     def daily_report_params
--- spec/controllers/daily_reports_controller_spec.rb
+++ spec/controllers/daily_reports_controller_spec.rb
@@ -62,4 +62,91 @@ describe DailyReportsController do
     end
   end
 
+  describe 'GET #edit' do
+
+    before :each do
+      @daily_report = create(:daily_report)
+      get :edit, id: @daily_report, monthly_report_id: @daily_report.monthly_report,
+        user_id: @daily_report.monthly_report.user
+    end
+
+    it "assigns the requested daily report to @daily_report" do
+      expect(assigns(:daily_report)).to eq @daily_report
+    end
+
+    it "renders the :edit template" do
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe 'PATCH #update' do
+
+    before :each do
+      @daily_report = create(:daily_report)
+    end
+
+    context "with valid attributes" do
+
+      before :each do
+        patch :update, id: @daily_report, monthly_report_id: @daily_report.monthly_report,
+          user_id: @daily_report.monthly_report.user,
+          daily_report: attributes_for(:daily_report, pomodori: 20, task: 20)
+      end
+
+      it "changes @daily_report's attributes" do
+        @daily_report.reload
+        expect(@daily_report.pomodori).to eq 20
+        expect(@daily_report.task).to eq 20
+      end
+
+      it "redirects to monthly_reports#show" do
+        expect(response).to redirect_to user_monthly_report_path(
+          @daily_report.monthly_report.user,
+          @daily_report.monthly_report
+        )
+      end
+    end
+
+    context "with invalid attributs" do
+
+      before :each do
+        patch :update, id: @daily_report, monthly_report_id: @daily_report.monthly_report,
+          user_id: @daily_report.monthly_report.user,
+          daily_report: attributes_for(:daily_report, pomodori: nil, task: nil)
+      end
+
+      it "does not changes @daily_report's attributes" do
+        @daily_report.reload
+        expect(@daily_report.pomodori).to_not eq nil
+        expect(@daily_report.task).to_not eq nil
+      end
+
+      it "re-renders the :edit template" do
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+
+    before :each do
+      @daily_report = create(:daily_report)
+    end
+    
+    it "deletes the daily report" do
+      expect{
+        delete :destroy, id: @daily_report, monthly_report_id: @daily_report.monthly_report,
+          user_id: @daily_report.monthly_report.user
+      }.to change(DailyReport, :count).by(-1)
+    end
+
+    it "redirects to monthly_report#show" do
+      delete :destroy, id: @daily_report, monthly_report_id: @daily_report.monthly_report,
+        user_id: @daily_report.monthly_report.user
+      expect(response).to redirect_to user_monthly_report_path(
+        @daily_report.monthly_report.user,
+        @daily_report.monthly_report
+      )
+    end
+  end
 end
